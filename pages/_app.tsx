680c8d174fc471069f8d1197cd2d5963d07888af
Harish1031 <harishkumarm415@gmail.com>
2024-12-27 23:16:49 +0530

    message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import { InstantDbProvider } from '@instantdb/react';
-import '../styles/globals.css';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
